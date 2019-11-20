07093f92736c494484a0f46f931b6c7e3ad73727
havidalbar@gmail.com <havidalbar@gmail.com>
2019-11-20 18:06:08 +0700

    Add Logout

--- app/src/main/java/com/example/delsa/fragment/ProfilFragment.java
+++ app/src/main/java/com/example/delsa/fragment/ProfilFragment.java
@@ -1,6 +1,7 @@
 package com.example.delsa.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.delsa.R;
+import com.example.delsa.activities.MainActivity;
+import com.example.delsa.activities.MainUserActivity;
+import com.example.delsa.activities.MenuActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfilFragment extends Fragment {
+public class ProfilFragment extends Fragment implements View.OnClickListener {
+
+    private Button btnLogout;
 
 
     public ProfilFragment() {
@@ -26,7 +35,24 @@ public class ProfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
+
+        btnLogout = view.findViewById(R.id.btn_logout);
+        btnLogout.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_logout:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(getActivity(), "Logout", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+                getActivity().finishAffinity();
+                break;
+
+        }
+    }
 }

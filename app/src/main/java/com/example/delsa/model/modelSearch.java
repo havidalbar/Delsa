6ea041c746522b3472d1492ad892445b3bea4eb0
Asrina <asrinafr@gmail.com>
2019-11-26 04:16:17 +0700

    add activity search layout

---
+++ app/src/main/java/com/example/delsa/model/modelSearch.java
@@ -0,0 +1,47 @@
+package com.example.delsa.model;
+
+public class modelSearch {
+    private String judul, link, hari, deskripsi;
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getHari() {
+        return hari;
+    }
+
+    public void setHari(String hari) {
+        this.hari = hari;
+    }
+
+    public String getDeskripsi() {
+        return judul;
+    }
+
+    public void setDeskripsi(String judul) {
+        this.judul = judul;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    private int photo;
+}

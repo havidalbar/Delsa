d6ba76041a4429bf5d14334c1c3a7b944baabbfa
radityarin <radityarin@Radityas-iMac.local>
2019-12-02 05:28:19 -0800

    change donasi pakaian activity from include to inside the layout

--- app/src/main/java/com/example/delsa/activities/DonasiPakaianActivity.java
+++ app/src/main/java/com/example/delsa/activities/DonasiPakaianActivity.java
@@ -3,11 +3,15 @@ package com.example.delsa.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
+import com.example.delsa.POJO.Donasi;
 import com.example.delsa.R;
 
 import static android.view.View.VISIBLE;
@@ -15,8 +19,10 @@ import static android.view.View.VISIBLE;
 public class DonasiPakaianActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Toolbar toolbar;
-    private LinearLayout ll_adddonasipakaian, ll_tidakadaform, ll_itemdonasi2;
-    private Button btn_adddonasipakaian;
+    private LinearLayout ll_adddonasipakaian, ll_tidakadaform, ll_itemdonasi2, btn_hapus1, btn_hapus2;
+    private Button btn_adddonasipakaian, btn_lanjutkandonasipakaian;
+    private Spinner spn_kategori1, spn_kategori2;
+    private EditText et_jumlahitem1, et_jumlahitem2, et_deskripsibarang1, et_deskripsibarang2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,19 @@ public class DonasiPakaianActivity extends AppCompatActivity implements View.OnC
         ll_itemdonasi2 = findViewById(R.id.item_donasi_2);
         btn_adddonasipakaian = findViewById(R.id.btn_add_donasipakaian);
         btn_adddonasipakaian.setOnClickListener(this);
+        btn_lanjutkandonasipakaian = findViewById(R.id.btn_lanjutkandonasipakaian);
+        btn_lanjutkandonasipakaian.setOnClickListener(this);
+
+        btn_hapus1 = findViewById(R.id.ll_hapus1);
+        btn_hapus2 = findViewById(R.id.ll_hapus2);
+        spn_kategori1 = findViewById(R.id.spn_kategori1);
+        spn_kategori2 = findViewById(R.id.spn_kategori2);
+
+        et_jumlahitem1 = findViewById(R.id.et_jumlahitem1);
+        et_jumlahitem2 = findViewById(R.id.et_jumlahitem2);
+
+        et_deskripsibarang1 = findViewById(R.id.et_deskripsi1);
+        et_deskripsibarang2 = findViewById(R.id.et_deskripsi2);
 
     }
 
@@ -47,6 +66,10 @@ public class DonasiPakaianActivity extends AppCompatActivity implements View.OnC
                 } else if (ll_adddonasipakaian.getVisibility() == VISIBLE){
                     ll_itemdonasi2.setVisibility(VISIBLE);
                 }
+                break;
+            case R.id.btn_lanjutkandonasipakaian:
+                Intent intent = new Intent(DonasiPakaianActivity.this, PenjemputanBarangActivity.class);
+                startActivity(intent);
         }
     }
 }

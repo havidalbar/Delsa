8f9e49624bfc5510f1f4e126ff99689050222085
Yoga Darma <kadekyoga125@gmail.com>
2019-12-03 03:53:06 +0700

    add onboarding screen

---
+++ app/src/main/java/com/example/delsa/adapter/SliderAdapter.java
@@ -0,0 +1,59 @@
+package com.example.delsa.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.delsa.R;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    public int[] slide_images = {
+            R.drawable.search, R.drawable.delivery, R.drawable.messagesent
+    };
+
+
+    @Override
+    public int getCount() {
+        return slide_images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == (RelativeLayout) o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = view.findViewById(R.id.imageTutor);
+
+        slideImageView.setImageResource(slide_images[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((RelativeLayout)object);
+    }
+}
